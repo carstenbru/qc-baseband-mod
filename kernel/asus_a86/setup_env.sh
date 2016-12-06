0a409c0356a7f1530bd131308fc484323bcbba05
Carsten Bruns <cbruns@seemoo.tu-darmstadt.de>
2016-12-06 19:38:12 +0100

    asus A86 kernel build environment

---
+++ kernel/asus_a86/setup_env.sh
@@ -0,0 +1,11 @@
+export NDK_ROOT=/home/carsten/Android/dev/android-ndk-r11c/
+
+export CC=$(pwd)/buildtools/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/buildtools/arm-eabi-4.7/bin/arm-eabi-
+
+export ARCH=arm
+export SUBARCH=arm
+
+export ASUS_BUILD_PROJECT=A86
+
+export QC_BASEBAND_SETUP_ENV=1
